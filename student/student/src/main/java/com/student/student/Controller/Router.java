fe3a505a31e7d3a558cc12d6ba967d806ac02f5e
Karan-Kumar-Mishra <emiltokaranmishra@gmail.com>
2025-07-01 20:00:39 +0530

    add the loombook

--- student/student/src/main/java/com/student/student/Controller/Router.java
+++ student/student/src/main/java/com/student/student/Controller/Router.java
@@ -1,11 +1,17 @@
 package com.student.student.Controller;
 
 import java.util.*;
+
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,37 +19,60 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/student")
 public class Router {
-    
-    @Autowired
-    private Repo repo;
 
-    @Autowired
-    private Control ctrl;
+  @Autowired
+  private Repo repo;
 
-    @GetMapping
-    public List<Student> getAll()
-    {
-      return ctrl.getAll();
+  @Autowired
+  private Control ctrl;
+
+  @GetMapping
+  public ResponseEntity<?> getAll() {
+    List<Student> all = ctrl.getAll();
+    if (all != null && !all.isEmpty()) {
+      return new ResponseEntity<>(all, HttpStatus.OK);
+    } else {
+      return new ResponseEntity<>(all, HttpStatus.NOT_FOUND);
+    }
+  }
+
+  @PostMapping
+  public ResponseEntity<Student> createEntry(@RequestBody Student st) {
+    try {
+      ctrl.saveEntry(st);
+      return new ResponseEntity<Student>(st, HttpStatus.CREATED);
+    } catch (Exception e) {
+      return new ResponseEntity<Student>(st, HttpStatus.BAD_REQUEST);
     }
 
-    @PostMapping
-    public boolean createEntry(@RequestBody Student st)
-    {
-     ctrl.saveEntry(st);
-     return true;
-    }
+  }
 
-
-    @PostMapping("id/{myId}")
-    public Optional<Student> getStudentById(@PathVariable String myId)
-    {
-       return repo.findById(myId);
+  @PostMapping("id/{myId}")
+  public ResponseEntity<Student> getStudentById(@PathVariable String myId) {
+    Optional<Student> stuEntry = ctrl.findBYId(myId);
+    if (stuEntry.isPresent()) {
+      return new ResponseEntity<Student>(stuEntry.get(), HttpStatus.OK);
+    } else {
+      return new ResponseEntity<Student>(HttpStatus.NOT_FOUND);
     }
+  }
 
-    @DeleteMapping("id/{myId}")
-    public Boolean deleteStudent(@PathVariable Object myId)
-    {
-      repo.deleteById(myId.toString());
-      return true;
+  @PutMapping
+  public Student updateById(@PathVariable ObjectId id, @RequestBody Student newEntry) {
+    Student old = repo.findById(id.toString()).orElse(null);
+    if (old != null) {
+      old.setTitle(
+          newEntry.getTitle() != null && newEntry.getTitle().equals("") ? newEntry.getTitle() : old.getTitle());
+      old.setContent(newEntry.getContent() != null && !newEntry.equals("") ? newEntry.getContent() : old.getContent());
     }
+    ctrl.saveEntry(old);
+    return old;
+  }
+
+  @DeleteMapping("id/{myId}")
+  public Boolean deleteStudent(@PathVariable Object myId) {
+    repo.deleteById(myId.toString());
+    return true;
+  }
+
 }
--- student/student/src/main/java/com/student/student/Controller/Student.java
+++ student/student/src/main/java/com/student/student/Controller/Student.java
@@ -3,37 +3,26 @@ package com.student.student.Controller;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 
 @Document(collection = "student")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Data
+
 public class Student {
     
     @Id
     private String id;
     private String title;
     private String content;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getcontent() {
-        return content;
-    }
-
-    public void setcontent(String content) {
-        this.content = content;
-    }
-
 }
